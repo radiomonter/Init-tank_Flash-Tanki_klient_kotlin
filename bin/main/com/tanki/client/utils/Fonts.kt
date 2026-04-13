74ae7bfd02cf0127f9c4e38d2bdd6f59a152bb1d
radio <radiomonter@gmail.com>
2026-04-13 22:59:43 +0200

    chore: remove bin/ from tracking, update .gitignore

--- bin/main/com/tanki/client/utils/Fonts.kt
+++
@@ -1,9 +0,0 @@
-package com.tanki.client.utils
-
-import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.tanki.client.utils.FontGenerator
-import com.badlogic.gdx.graphics.Color
-
-fun makeFont(size: Int): BitmapFont {
-    return FontGenerator.makeFont(size)
-}
